574a1dc637d0e4203f40bca000b69ab3efd86530
aq222486 <aq222486@DROPIN_86>
2023-10-17 12:33:29 +0100

    Add project files.

---
+++ Nea project/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+namespace Nea_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var game = new Nea_project.Game1();
+            game.Run();
+        }
+    }
+    class tile
+    {
+
+    }
+}
