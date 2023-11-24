418ffb0ba147740eaa512ab4130fd4ea264cb5c8
Dylan Cocker <AQ222486@aquinas.ac.uk>
2023-11-24 10:24:39 +0000

    added omment

--- Nea project/Game1.cs
+++ Nea project/Game1.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Nea_project
 {
     public class Game1 : Game
     {
+        Texture2D squareTexture;
+        double gamestate = 1.5;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        List<tile> tiles = new List<tile>();
 
         public Game1()
         {
@@ -18,16 +23,27 @@ namespace Nea_project
 
         protected override void Initialize()
         {
+            
+            int row = 0;
+            int col = 0;    
             // TODO: Add your initialization logic here
-
+            for (int i = -1; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    row = i*55; col = j*55;
+                    tile newtile = new tile(0, 0, 0, 0, row, col);// creates new tile object
+                    tiles.Add(newtile);// adds it to list 
+                }
+            }
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            squareTexture = Content.Load<Texture2D>("grass");//loads grass 
 
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +51,39 @@ namespace Nea_project
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+           
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+
+            if(gamestate == 1.5 )
+            {
+                int col = -55;
+                int row = 0;
+                GraphicsDevice.Clear(Color.CornflowerBlue);//sets abck gorund to vue
+                _spriteBatch.Begin();// begins draws  in the srpites 
+                for (int i = 0; i < 10; i++)//gennnerates grass tiles in grid
+                {
+                    col += 55;
+                    row = 0;
+                    for (int j = 0; j < 15; j++)
+                    {
+
+                        _spriteBatch.Draw(squareTexture, new Vector2(row, col), Color.White);
+                        row += 55;
+
+                    }
+
+                }
+
+                _spriteBatch.End();
+            }
+            
+
 
             base.Draw(gameTime);
         }
--- Nea project/Program.cs
+++ Nea project/Program.cs
@@ -1,17 +1,59 @@
 ﻿
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 using System;
+using Microsoft.Xna.Framework.Input;
+
 namespace Nea_project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            
             using var game = new Nea_project.Game1();
             game.Run();
         }
     }
-    class tile
-    {
 
+   class tank
+   {
+        //this si for luke 
+
+   }
+    
+    
+    class tile //basic class for the tiles 
+    {
+        Texture2D grassTexture;
+        private int _x;// x position
+        public int X {get { return _x;}}
+        private int _y;// y postion 
+        public int Y { get { return _y; } }
+        private int _type;// what type as in grass foret moutain road
+        public int Type { get { return _type; } }
+
+        private int _movemod;// how ti modifies the movemnt of tanks like -1 or +"
+        public int Movemod { get { return _movemod;} }
+
+        private int _defmod;// if the teraain adds defense 
+       public int Defmod { get { return _defmod; } }
+        private int _accmod;//if it deacrease or increase attack 
+        public int Accmod { get { return _accmod; } }
+        public tile (int  type, int movemod, int defmod,int ACCmod,int x , int y)//constructure
+        {
+            _type = type;
+            _movemod = movemod;
+            _defmod = defmod;
+            _accmod = ACCmod;
+            _x = x;
+            _y = y;
+        }
+       
+        
     }
+    
+    }
+
 }
